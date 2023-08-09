7acff4d3be95f1b2b66cdc3063f2034b2ca34215
dzhelil <dzhelil.murtazaev1@gmail.com>
2023-08-09 19:14:52 +0300

    first_commit

---
+++ send.sh
@@ -0,0 +1,10 @@
+STATUS=$(ceph health detail)
+LASTSTATUS=$(cat /tmp/laststatus.txt)
+
+if [ "$(ceph health detail)" != "$LASTSTATUS" ]
+then
+                curl -s -X POST https://api.telegram.org/"токен_бота"/sendMessage -d chat_id="id_чата" -d text="MY_CEPH 
+$STATUS"
+fi
+ceph health detail > /tmp/laststatus.txt
+
